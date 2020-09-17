b35b047c6d7e1f9287f749b757075336431d8445
Sumayyah Mariam <sumayyahmariam019@gmail.com>
2020-09-16 21:56:22 -0500

    Initialized 3 arrays with input questions for manager, engineer and intern

--- Develop/app.js
+++ Develop/app.js
@@ -7,29 +7,108 @@ const fs = require("fs");
 
 const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
-
 const render = require("./lib/htmlRenderer");
 
+//Manager questions
+const managerQues = [
+    {
+        type: "input",
+        name: "name",
+        message: "What's your Manager's name?"
+    },
+    {
+        type: "input",
+        name: "id",
+        message: "What's your Manager's ID?"
+    },
+    {
+        type: "input",
+        name: "email",
+        message: "What's your Manager's email?"
+    },
+    {
+        type: "input",
+        name: "officeNumber",
+        message: "What's your Manager's officeNumber?"
+    },
+    {
+        type: "list",
+        name: "addMember",
+        message: "Which team member would you like to add?",
+        choices: [
+            "Engineer",
+            "Intern",
+            "No more members to add"
+        ]
+    }
+];
 
-// Write code to use inquirer to gather information about the development team members,
-// and to create objects for each team member (using the correct classes as blueprints!)
+//Engineer ques
+const engQues = [
+    {
+        type: "input",
+        name: "name",
+        message: "What's your Engineer's name?"
+    },
+    {
+        type: "input",
+        name: "id",
+        message: "What's your Engineer's ID?"
+    },
+    {
+        type: "input",
+        name: "email",
+        message: "What's your Engineer's email address?"
+    },
+    {
+        type: "input",
+        name: "github",
+        message: "What's your engineer's github username?"
+    },
+    {
+        type: "list",
+        name: "addMember",
+        message: "Which team member would you like to add?",
+        choices: [
+            "Engineer",
+            "Intern",
+            "No more members to add"
+        ]
+    }
+];
 
-// After the user has input all employees desired, call the `render` function (required
-// above) and pass in an array containing all employee objects; the `render` function will
-// generate and return a block of HTML including templated divs for each employee!
+//Intern ques
+const internQues = [
+    {
+        type: "input",
+        name: "name",
+        message: "What's your Intern's name?"
+    },
+    {
+        type: "input",
+        name: "id",
+        message: "What's your Intern's ID?"
+    },
+    {
+        type: "input",
+        name: "email",
+        message: "What's your Intern's email address?"
+    },
+    {
+        type: "input",
+        name: "school",
+        message: "What is your Intern's school?"
+    },
+    {
+        type: "list",
+        name: "addMember",
+        message: "Which team member would you like to add?",
+        choices: [
+            "Engineer",
+            "Intern",
+            "No more members to add"
+        ]
+    }
+];
 
-// After you have your html, you're now ready to create an HTML file using the HTML
-// returned from the `render` function. Now write it to a file named `team.html` in the
-// `output` folder. You can use the variable `outputPath` above target this location.
-// Hint: you may need to check if the `output` folder exists and create it if it
-// does not.
 
-// HINT: each employee type (manager, engineer, or intern) has slightly different
-// information; write your code to ask different questions via inquirer depending on
-// employee type.
-
-// HINT: make sure to build out your classes first! Remember that your Manager, Engineer,
-// and Intern classes should all extend from a class named Employee; see the directions
-// for further information. Be sure to test out each class and verify it generates an
-// object with the correct structure and methods. This structure will be crucial in order
-// for the provided `render` function to work! ```
